47bfdf7762afcee1d27291c74e4e42ee4f6006fa
Fernando Mendes <fernando.mendes@xpi.com.br>
2019-10-24 09:18:08 -0300

    Adicionando a solução do exemplo de Hangfire

---
+++ Hangfire.API/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire.Mongo;
+using Hangfire.Domain;
+
+namespace Hangfire.API
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var migrationOptions = new MongoMigrationOptions
+            {
+                Strategy = MongoMigrationStrategy.Migrate,
+                BackupStrategy = MongoBackupStrategy.Collections
+            };
+            var storageOptions = new MongoStorageOptions
+            {
+                MigrationOptions = migrationOptions
+            };
+
+            services.AddHangfire(x => 
+                x.UseMongoStorage("mongodb://localhost:17017", "Hangfire", storageOptions));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Exemplo Hangfire - github.com/marraia");
+            });
+
+            InitProcess();
+        }
+
+        private void InitProcess()
+        {
+            var division = new Division();
+            RecurringJob.AddOrUpdate(() => division.DivisionRandom(), Cron.Minutely());
+        }
+    }
+}
---
+++ Hangfire.Domain/Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hangfire.Domain
+{
+    public class Division
+    {
+        public void DivisionRandom()
+        {
+            try
+            {
+                var number1 = new Random().Next(0, 10);
+                var number2 = new Random().Next(0, 10);
+
+                var result = number1 / number2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new Exception($"Divisão por zero {ex.Message}");
+            }
+        }
+    }
+}
